376e50854f4cef34558c60188f6a31385bb103e4
David Lin <davidlin15cute@outlook.com>
2022-03-02 21:25:36 -0500

    Update 1322-ads-performance.sql

--- 1322-ads-performance/1322-ads-performance.sql
+++ 1322-ads-performance/1322-ads-performance.sql
@@ -1,6 +1,8 @@
 # Write your MySQL query statement below
 select ad_id, 
+# here u have to use sum, count will only return the whole column
 ifnull(round(sum(action = 'Clicked') / sum(action = 'Clicked' or action = 'Viewed') * 100, 2), 0) as ctr
 from Ads
 group by ad_id
+# u have to follow this, cause ctr has the desc  order
 order by ctr desc, ad_id
