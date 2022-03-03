8b621edfbf0902f90bdbdc7d9088d839a4f75378
David Lin <davidlin15cute@outlook.com>
2022-03-02 21:03:51 -0500

    Update 1303-find-the-team-size.sql

--- 1303-find-the-team-size/1303-find-the-team-size.sql
+++ 1303-find-the-team-size/1303-find-the-team-size.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
 SELECT employee_id, 
     COUNT(team_id) OVER(PARTITION BY team_id) AS team_size
+# window function is a powerful way for groupby no need another
 FROM Employee
