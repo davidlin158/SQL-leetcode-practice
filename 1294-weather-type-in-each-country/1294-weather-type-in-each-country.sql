a2743716125771413c04880cd43450b7b5287b0c
David Lin <davidlin15cute@outlook.com>
2022-03-02 20:48:32 -0500

    Update 1294-weather-type-in-each-country.sql

--- 1294-weather-type-in-each-country/1294-weather-type-in-each-country.sql
+++ 1294-weather-type-in-each-country/1294-weather-type-in-each-country.sql
@@ -1,10 +1,12 @@
 # Write your MySQL query statement below
+# when avg it automatically sum no need of avg(sum())
 SELECT a.country_name,
 CASE WHEN AVG(weather_state)<=15 THEN "Cold"
 WHEN AVG(weather_state)>=25 THEN "Hot"
 ELSE "Warm" END as weather_type FROM Countries as a
 JOIN Weather as b
 ON a.country_id=b.country_id
+#faster way to find date
 WHERE Year(b.day) = 2019 
 AND Month(b.day) = 11 
 GROUP BY a.country_id;
