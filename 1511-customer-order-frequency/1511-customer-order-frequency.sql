f59dcc26894791ec00d16f2e147e8668e2aac587
David Lin <davidlin15cute@outlook.com>
2022-03-04 11:23:39 -0500

    Update 1511-customer-order-frequency.sql

--- 1511-customer-order-frequency/1511-customer-order-frequency.sql
+++ 1511-customer-order-frequency/1511-customer-order-frequency.sql
@@ -4,9 +4,11 @@ SELECT
     customer_id,
     name
 FROM Customers 
+# get use to using, instead of just single line
 JOIN Orders USING(customer_id) 
 JOIN Product USING(product_id)
 GROUP BY customer_id
+# having + if here can solve it. important
 HAVING 
     SUM(
         IF(MONTH(order_date) = 6, quantity, 0) * price
