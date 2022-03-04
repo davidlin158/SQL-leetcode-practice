a9b82be7d8be5bb853b03bdf19b6595b1963b634
David Lin <davidlin15cute@outlook.com>
2022-03-04 00:53:02 -0500

    Update 1484-group-sold-products-by-the-date.sql

--- 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
+++ 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
@@ -2,6 +2,7 @@
 SELECT 
     sell_date,
     COUNT(DISTINCT product) AS num_sold,
+    #use group concat here, to get all the word in one cell. sep automatically is ',' and order by is asc automatically as well.
     GROUP_CONCAT(DISTINCT product) AS products
 FROM Activities
 GROUP BY sell_date;
