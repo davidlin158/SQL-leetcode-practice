9eb1786187a0349511dcaa584c6d362bacb8d0a1
David Lin <davidlin15cute@outlook.com>
2022-03-04 00:34:30 -0500

    Update 1407-top-travellers.sql

--- 1407-top-travellers/1407-top-travellers.sql
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
 SELECT DISTINCT u.name, 
+# here since donald doesn't have any ride, it will return null, use ifnull to avoid.
 IFNULL(SUM(distance) OVER (PARTITION BY user_id), 0) as travelled_distance 
 FROM Rides r RIGHT JOIN Users u ON r.user_id = u.id 
 ORDER BY travelled_distance DESC, name
