e02a37e7fc1074394868ccfb1caea3efe10bcae3
David Lin <davidlin15cute@outlook.com>
2022-03-04 16:25:27 -0500

    Time: 2736 ms (19.82%), Space: 0B (100.00%) - LeetHub

---
+++ 1623-all-valid-triplets-that-can-represent-a-country/1623-all-valid-triplets-that-can-represent-a-country.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT
+    a.student_name AS member_A,
+    b.student_name AS member_B,
+    c.student_name AS member_c
+FROM schoolA a
+JOIN schoolB b
+ON a.student_name != b.student_name AND a.student_id != b.student_id
+JOIN schoolC c
+# because there are same tom and same id.
+ON b.student_name != c.student_name AND a.student_name != c.student_name
+AND b.student_id != c.student_id AND a.student_id != c.student_id
