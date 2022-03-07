ec717c6471ac2029e2d4a50aafe5d54fc7cabd09
David Lin <davidlin15cute@outlook.com>
2022-03-07 16:19:04 -0500

    Time: 696 ms (7.10%), Space: 0B (100.00%) - LeetHub

--- 177-nth-highest-salary/177-nth-highest-salary.sql
+++ 177-nth-highest-salary/177-nth-highest-salary.sql
@@ -1,10 +1,13 @@
 # create function
-CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+
+CREATE FUNCTION getNthHighestSalary(N INT) 
+RETURNS INT
 BEGIN
 DECLARE M INT;
 SET M=N-1;
   RETURN (
       # Write your MySQL query statement below.
-      SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT M, 1
+      SELECT distinct Salary FROM Employee ORDER BY Salary DESC LIMIT M, 1
   );
 END
+
