ac616cf79f1a620afe0b0bbc626028b473e60e3f
David Lin <davidlin15cute@outlook.com>
2022-03-07 16:23:00 -0500

    Time: 310 ms (75.38%), Space: 0B (100.00%) - LeetHub

--- 177-nth-highest-salary/177-nth-highest-salary.sql
+++ 177-nth-highest-salary/177-nth-highest-salary.sql
@@ -7,7 +7,7 @@ DECLARE M INT;
 SET M=N-1;
   RETURN (
       # Write your MySQL query statement below.
-      SELECT distinct Salary FROM Employee ORDER BY Salary DESC LIMIT M, 1
+      SELECT distinct Salary FROM Employee ORDER BY Salary DESC LIMIT 1 offset m
   );
 END
 
