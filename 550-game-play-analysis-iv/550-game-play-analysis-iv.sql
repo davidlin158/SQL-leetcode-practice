3878d378c606a9c8f8adeb4b731ec41973f3f723
David Lin <davidlin15cute@outlook.com>
2022-03-08 16:17:59 -0500

    Time: 713 ms (20.51%), Space: 0B (100.00%) - LeetHub

---
+++ 550-game-play-analysis-iv/550-game-play-analysis-iv.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+#here is to check is true or false if min(event_date) over(partition by player_id) + 1= event_date
+with cte as (select player_id, min(event_date) over(partition by player_id) + 1= event_date as is_return from activity)
+select round(sum(is_return)/count(distinct player_id), 2)  as fraction from cte
