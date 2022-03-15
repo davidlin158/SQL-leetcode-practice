ab2962d91346fa20af4840ad0eaffb4c53816bb3
David Lin <davidlin15cute@outlook.com>
2022-03-15 18:31:18 -0400

    Time: 454 ms (29.04%), Space: 0B (100.00%) - LeetHub

---
+++ 1285-find-the-start-and-end-number-of-continuous-ranges/1285-find-the-start-and-end-number-of-continuous-ranges.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+
+
+
+select log_start.log_id as START_ID, log_end.log_id as END_ID from 
+    (select log_id from logs where log_id - 1 not in (select * from Logs)) log_start,
+    (select log_id from logs where log_id + 1 not in (select * from Logs)) log_end
+    where log_start.log_id <= log_end.log_id
+    group by START_ID;
